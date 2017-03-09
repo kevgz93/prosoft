20bb28251dbf4164fa272ad5de8b21ad2028fba2
AMERICAS\guerreke <kguerreroz93@gmail.com>
2017-03-09 15:28:05 -0600

    Second page Calendar in process

--- js/angular/controller/datectrl.js
+++ js/angular/controller/datectrl.js
@@ -1,3 +1,15 @@
-angular.module('MyApp',['$scope','ngMaterial', 'ngMessages', 'material.svgAssetsCache']).controller('AppCtrl', function($scope) {
-  $scope.myDate = new Date();
+var app = angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
+
+app.controller('AppCtrl', function() {
+  this.myDate = new Date('01/01/2008');
+});
+
+
+app.controller('drop', function($scope) {
+    $scope.names = ["Emil", "Tobias", "Linus"];
+});
+
+app.controller("calendarCtrl", function($scope, $filter, $http, $q) {
+
+
 });
